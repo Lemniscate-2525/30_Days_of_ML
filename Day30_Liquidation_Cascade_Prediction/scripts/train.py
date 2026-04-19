886d13366d5d778874f736a674e6737c4720f685
Akshat Mishra <akshat.mishra2525@gmail.com>
2026-04-19 17:41:28 +0530

    Create train.py

---
+++ Day30_Liquidation_Cascade_Prediction/scripts/train.py
@@ -0,0 +1,53 @@
+import time
+import torch
+import torch.nn as nn
+import sys
+import os
+
+# Adding parent directory to path so we can import 'core'
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from core.config import *
+from core.data import get_dataloaders
+from core.model import LlamaModel
+
+def main():
+  
+    print(f"[SYSTEM] Initializing Training on {device}")
+    train_loader, _ = get_dataloaders()
+    
+    raw_model = LlamaModel(vocab_size, d_model, num_layers, num_heads, seq_len).to(device)
+  
+    try:
+        model = torch.compile(raw_model)
+      
+    except Exception:
+        model = raw_model
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr = lr, weight_decay = 1e-2)
+    criterion = nn.BCEWithLogitsLoss()
+
+    for e in range(epochs):
+      
+        model.train()
+        total_loss = 0
+        start = time.time()
+        
+        for X_batch, y_batch in train_loader:
+          
+            X_batch, y_batch = X_batch.to(device), y_batch.to(device)
+            optimizer.zero_grad()
+          
+            loss = criterion(model(X_batch), y_batch)
+            loss.backward()
+            optimizer.step()
+          
+            total_loss += loss.item()
+            
+        print(f"Epoch {e+1:02d}/{EPOCHS} | Loss: {total_loss/len(train_loader):.4f} | Time: {time.time()-start:.2f}s")
+
+    # Saving trained weights : 
+    torch.save(raw_model.state_dict(), model_save_path)
+  
+if __name__ == "__main__":
+    main()
