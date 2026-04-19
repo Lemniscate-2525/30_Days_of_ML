36bfd4459d5659829220286fe496547e116ff2ad
Akshat Mishra <akshat.mishra2525@gmail.com>
2026-04-19 17:27:37 +0530

    Create config.py

---
+++ Day30_Liquidation_Cascade_Prediction/core/config.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import time
+import os
+import math
+
+from sklearn.metrics import roc_auc_score, precision_recall_curve, average_precision_score
+
+from collections import Counter
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.utils.data import DataLoader, Dataset
+
+# Device Setup & Configuration :  
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Hyperparameters : 
+batch_size = 32
+epochs = 5
+lr = 3e-4
+
+# Architecture : 
+seq_len = 128     
+d_model = 256
+num_heads = 8
+num_layers = 4
+vocab_size = 100  
