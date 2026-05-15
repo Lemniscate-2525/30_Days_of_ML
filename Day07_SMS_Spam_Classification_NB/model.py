c82ba9a733d96f5b08629ec6b62b3a50115effbd
Akshat Mishra <akshat.mishra2525@gmail.com>
2026-05-15 17:45:51 +0530

    Update model.py

--- Day07_SMS_Spam_Classification_NB/model.py
+++ Day07_SMS_Spam_Classification_NB/model.py
@@ -23,7 +23,7 @@ from sklearn.metrics import roc_auc_score
 from sklearn.metrics import roc_curve
 
 # Dataset :
-file_path = "/content/spam.csv"
+file_path = "/content/spam - Copy.csv"
 df = pd.read_csv(file_path, encoding = 'latin-1')
 
 # Text Preprocessing :
@@ -32,23 +32,23 @@ df.columns = ['label', 'text']
 
 df['label'] = df['label'].map({'spam' : 0, 'ham' : 1})
 
-# Text-EDA(NLP like) : 
+# Text-EDA : 
 sns.countplot(x = df['label'])   # Class Dist Ham/Spam : 
 plt.title("Class Dist : ")
 plt.show()
 
 df['length'] = df['text'].apply(len)  # Message Len Dist : 
 
-sns.histplot(df[df['label'] == 0]['length'], color = 'blue', label = 'Ham', kde = True)
-sns.histplot(df[df['label'] == 1]['length'], color = 'red', label = "Spam", kde = True)
+sns.histplot(df[df['label'] == 0]['length'], color = 'red', label = 'Spam', kde = True)
+sns.histplot(df[df['label'] == 1]['length'], color = 'blue', label = "Ham", kde = True)
 plt.legend()
 plt.title("Message Len Dist : ")
 plt.show()
 
 df['text'] = df['text'].astype(str)  # Clean Dataset 
 
-spam_text = " ".join(df[df['label'] == 1]['text'].tolist())
-ham_text =  " ".join(df[df['label'] == 0]['text'].tolist())
+spam_text = " ".join(df[df['label'] == 0]['text'].tolist())
+ham_text =  " ".join(df[df['label'] == 1]['text'].tolist())
 
 print("Spam Len :", len(spam_text))
 print("Ham Len :", len(ham_text))
@@ -85,7 +85,7 @@ train_NBC = time.time() - start # Training Time
 
 start = time.time() 
 pred_nbc = NBC.predict(X_test) # Predictions
-prob_nbc = NBC.predict_proba(X_test)[:, 1] # Probabilities
+prob_nbc = NBC.predict_proba(X_test)[:, 0] # Probabilities
 inf_nbc = (time.time() - start)/X_test.shape[0] # Inference Time
 
 # Training : (BBC)
@@ -96,7 +96,7 @@ train_BBC = time.time() - start
 
 start = time.time()
 pred_bbc = BBC.predict(X_test)
-prob_bbc = BBC.predict_proba(X_test)[:, 1] 
+prob_bbc = BBC.predict_proba(X_test)[:, 0] 
 inf_bbc = (time.time() - start)/X_test.shape[0] 
 
 # Training : (Log-Reg)
@@ -107,19 +107,19 @@ train_clf = time.time() - start
 
 start = time.time()
 pred_lr = clf.predict(X_test)
-prob_lr = clf.predict_proba(X_test)[:, 1] 
+prob_lr = clf.predict_proba(X_test)[:, 0] 
 inf_lr = (time.time() - start)/X_test.shape[0]
 
 
 # Metrics : 
 def evaluate(name, y_true, y_pred, y_prob = None):
     print(f"\n{name}")
-    print("Classification Report : ", classification_report(y_true, y_pred))
+    print(classification_report(y_true, y_pred))
     if y_prob is not None:
-        print("ROC AUC:", roc_auc_score(y_true, y_prob))
+        print("ROC AUC:", roc_auc_score(y_true == 0, y_prob))
 
 evaluate("Multinomial NB", y_test, pred_nbc, prob_nbc)
-evaluate("Bernoulli NB", y_test, pred_bbc)
+evaluate("Bernoulli NB", y_test, pred_bbc, prob_bbc)
 evaluate("Logistic Regression", y_test, pred_lr, prob_lr)
 
 # Confusion Matrix : 
@@ -129,8 +129,8 @@ plt.title("Confusion Matrix :(Multinomial BC) ")
 plt.show()
 
 # ROC Comparison : 
-fpr_nb, tpr_nb, _ = roc_curve(y_test, prob_nbc)
-fpr_lr, tpr_lr, _ = roc_curve(y_test, prob_lr)
+fpr_nb, tpr_nb, _ = roc_curve(y_test == 0, prob_nbc)
+fpr_lr, tpr_lr, _ = roc_curve(y_test == 0, prob_lr)
 
 plt.plot(fpr_nb, tpr_nb, label = "Naive Bayes(Multinomial) : ")
 plt.plot(fpr_lr, tpr_lr, label = "Logistic Regression : ")
@@ -141,7 +141,7 @@ plt.show()
 
 # Top Spam Indicating Feature Names : 
 feature_names = tfidf.get_feature_names_out()
-spam_wts = NBC.feature_log_prob_[1]
+spam_wts = NBC.feature_log_prob_[0]
 
 top = np.argsort(spam_wts)[-20:]
 plt.barh(feature_names[top], spam_wts[top])
@@ -156,4 +156,3 @@ print("LR :", train_clf)
 print("\nInference Latency : ")
 print("NB :", inf_nbc)
 print("LR :", inf_lr)
-
